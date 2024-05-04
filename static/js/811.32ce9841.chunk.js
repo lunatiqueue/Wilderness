dd25313780fa1454a81ffc737d124020528f8315
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-05-04 15:39:01 +0000

    Deploying to gh-pages from @ lunatiqueue/Wilderness@bae02df36a985ee651ee9a7c304bfa697725fb4c 🚀

---
+++ static/js/811.32ce9841.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkWilderness=self.webpackChunkWilderness||[]).push([[811],{811:function(e,n,s){s.r(n);var t=s(439),u=s(791),a=(s(18),s(184));n.default=function(){var e=(0,u.useState)(!1),n=(0,t.Z)(e,2),s=n[0],c=n[1];return(0,u.useEffect)((function(){var e=setTimeout(c(!0),1e3);return clearTimeout(e)}),[]),(0,a.jsx)("div",{className:"page ".concat(s?"show":""),children:"Mountains Page"})}},18:function(){}}]);
+//# sourceMappingURL=811.32ce9841.chunk.js.map
